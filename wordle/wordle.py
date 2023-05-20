afcfa2c095e7e752d9b7a2655b13ac1fe6976f36
JeikuDevvv <jakelourencevillar.dev@gmail.com>
2023-05-21 02:44:15 +0800

    from the start

--- wordle/wordle.py
+++ wordle/wordle.py
@@ -1,77 +1,33 @@
 import requests
 
-wordLength = 3 # Minimum Value
-maxGuesses = 5
-
 def main():
-    print("\n\t\t[ Welcome to Python Worlde by Jeiku Dev ]\n")
+    print("\n[ Welcome to Python Worlde by Jeiku Dev ]\n")
     
-    modeSelect = int(input("\tSelect Mode:\n\t[1] Easy (3 Letters) \n\t[2] Intermediate (5 Lettters) \n\t[3] Hard (7 Letters) \n\t[4] Custom \n\t-> "))
-    if modeSelect == 1:
-        modeValue = 3
-        secretWord = getSecretWord(modeValue)
-        
-    elif modeSelect == 2:
-        modeValue = 5
-        print(getSecretWord(modeValue))
-            
-    elif modeSelect == 3:
-        modeValue = 7
-        print(getSecretWord(modeValue))
-            
-    elif modeSelect == 4:
-            print('custom')
-    while True:
-        strFormSecretWord = "".join(ch for ch in secretWord if ch.isalnum())
-        print('\n\tI have thought up a Word.\n\t'
-            + 'You have {} guesses to get it.'.format(modeValue))
-            
-        wordGuesses = 1
-        while wordGuesses <= maxGuesses:
-            guess = ''
-            while len(guess) != modeValue or not guess.isdecimal():
-                guess = input("\n\tGuess #{} ".format(wordGuesses))
-            
-                clues = getClues(guess, strFormSecretWord)
-                print("\tClues: {}".format(clues))
-                wordGuesses += 1
-            wordGuesses += 1
-            
-            if guess == strFormSecretWord:
-                break
-            if wordGuesses > modeValue:
-                print("\n\tYou run out of guesses! The Answer was {} ".format(strFormSecretWord))
-        
-        response = input("\n\tDo you wanna play Again? (y/n/r) : ").lower()
-        if response == 'n':
-            break
-        if response == 'r':
-            main()
     
-    print("\n\tTHANK YOU FOR PLAYING!!!")
-
+    
 def getSecretWord(wordLength):
     response = requests.get('https://random-word-api.vercel.app/api?length={}'.format(wordLength))
     if response.status_code == requests.codes.ok:
-        return (response.text)
+        secretWord = "".join(ch for ch in response.text if ch.isalnum())
+        return secretWord
     else:
         print("Error:", response.status_code, response.text)
 
 
-def getClues(guess, strFormSecretWord):
-    if guess == strFormSecretWord:
+def getClues(guess, secretWord):
+    if guess == secretWord:
         return '\n\tYou got it!!'
 
     clues = []
 
     for i in range(len(guess)):
-        if guess[i] == strFormSecretWord[i]:
+        if guess[i] == secretWord[i]:
             clues.append('Letter Found in place!')
-        elif guess[i] in strFormSecretWord[i]:
+        elif guess[i] in secretWord[i]:
             clues.append('Letter Found!')
             
     if len(clues) == 0:
-        return 'Bagels'
+        return 'No Letter Found!'
     else:
         clues.sort()
         return ' '.join(clues)
