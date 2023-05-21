5b6403812e336d4d65b0ea5927dd2ae1bfa8fdb1
JeikuDevvv <jakelourencevillar.dev@gmail.com>
2023-05-21 17:57:35 +0800

    bagels finished

--- bagels/bagels.py
+++ bagels/bagels.py
@@ -10,15 +10,15 @@ maxGuesses = 10
 def main():
 
     print("\nI am thinking of a {} number. Try to guess what it is.\n\n".format(numberDigits)
-          + "When I say:   |  That's means:\n"
-          + "Pico          |  One digit is correct but in the wrong position.\n"
-          + "Fermi         |  One digit is correct and in the right position.\n"
-          + "Bagels        |  No digit is correct.\n\n")
+            + "When I say:   |  That's means:\n"
+            + "Pico          |  One digit is correct but in the wrong position.\n"
+            + "Fermi         |  One digit is correct and in the right position.\n"
+            + "Bagels        |  No digit is correct.\n\n")
 
     while True:
         secretNumber = getSecretNumber()
         print('\nI have thought up a number.\n'
-              + 'You have {} guesses to get it.'.format(maxGuesses))
+                + 'You have {} guesses to get it.'.format(maxGuesses))
 
         numberGuesses = 1
         while numberGuesses <= maxGuesses:
@@ -35,11 +35,11 @@ def main():
                 break
             if numberGuesses > maxGuesses:
                 print('\nYou ran our of guesses\n'
-                      + 'The Answer was {}.'.format(secretNumber))
+                        + 'The Answer was {}.'.format(secretNumber))
 
         if not input('\nDo you want to play again? (y/n): ').lower().startswith('y'):
             break
-        print('\nThanks for Playing <3.')
+    print('\nThanks for Playing <3.')
 
 
 def getSecretNumber():
@@ -61,8 +61,10 @@ def getClues(guess, secretNumber):
     for i in range(len(guess)):
         if guess[i] == secretNumber[i]:
             clues.append('Fermi')
-        elif guess[i] in secretNumber[i]:
+        elif guess[i] in secretNumber:
             clues.append('Pico')
+        # if guess[i] not in secretNumber:
+        #     clues.append('[0]')
 
     if len(clues) == 0:
         return 'Bagels'
